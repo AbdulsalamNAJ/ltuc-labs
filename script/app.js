d40978efae2b266b4eb922eec64126d08383d687
Abdulsalam Al-Najjar <83532131+AbdulsalamNAJ@users.noreply.github.com>
2021-05-24 07:13:07 +0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,10 @@
+let userName = prompt('What is your name?');
+console.log(userName);
+let userAge = prompt('What is the year of your birth?');
+console.log(userAge);
+let userCake = prompt('Which of these do you prefer the most?  |  Cakes  |  Cup cake  |  Ice cream cake');
+console.log(userCake);
+let userFlavor = prompt('What is your favorite flavor?  |  Vanilla  |  Almond  |  Pistachio Marble');
+console.log(userFlavor);
+
+alert(`Hello Mr./Ms. ${userName} your age is: ${2021 - userAge} and you choosed: ${userCake} with a flavor: ${userFlavor}`);
